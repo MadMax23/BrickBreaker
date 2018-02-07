d84f3690def852b1f7535b1c363955cacd7876b7
MadMax23 <36238560+MadMax23@users.noreply.github.com>
2018-02-07 10:12:35 -0700

    Add files via upload

---
+++ Assets/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+
+
+        //if our health gets to zero 
+        //then destroy this object    }
+    }
+}
+	
